565bcfe5d46507baf522a4e5d174bbadb324f375
Christopher MacGown <chris@glassbear.com>
2020-10-25 21:55:56 -0700

    At least update the name

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,22 +6,20 @@
 
 
 <script>
-
-  export default {
-    components: {
-    },
-    head(){
-      return {
-        title: "Lums - Vue Nuxt SEO Landing Template"
-      }
-    },
-    mounted(){
-      this.$nextTick(() => {
-        this.$nuxt.$loading.start()
-        setTimeout(() => this.$nuxt.$loading.finish(), 500)
-      })
+export default {
+  components: {},
+  head() {
+    return {
+      title: 'MacGown & Associates - Cloud Native leadership for modern tech teams',
     }
-  }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start()
+      setTimeout(() => this.$nuxt.$loading.finish(), 500)
+    })
+  },
+}
 </script>
 
 <style>
